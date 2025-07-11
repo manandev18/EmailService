9ef7955b35b59850ac2ba3269eb234525f0c67bb
manandev18 <andraskarmanan725@gmail.com>
2025-07-11 15:41:39 +0530

    cors issue

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3000;
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./docs/swagger.yaml");
+const cors = require("cors");
+app.use(cors());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
